651782d3d6c5b293d3fa6d065ac4487fdad6d915
hackerr3 <hacker.rajesh3@gmail.com>
2024-12-20 12:09:20 +0530

    first commit

---
+++ OtpGenerator/src/main/java/com/otp/OtpGenerator/OtpGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.otp.OtpGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpGeneratorApplication.class, args);
+	}
+
+}
